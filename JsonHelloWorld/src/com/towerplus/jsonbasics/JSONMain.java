6d9a112abad746bf5b7b043036fe5f4f766268bc
javamani <pioneermani@yahoo.com>
2022-05-15 17:51:52 -0400

    commit

---
+++ JsonHelloWorld/src/com/towerplus/jsonbasics/JSONMain.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.towerplus.jsonbasics;
+
+/**
+ * @author Administrator
+ *
+ */
+public class JSONMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		ObjectMapper jsonObjectMapper = new ObjectMapper();
+	}
+
+}
