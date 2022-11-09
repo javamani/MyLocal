9c63a7d8214c5f5374a97da4a9e41e62b7bcf2ff
javamani <pioneermani@yahoo.com>
2022-11-09 05:26:23 -0500

    Guhan demo changes

---
+++ GuhanDemo/src/main/java/com/pjpexercices/springboot/demo/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.pjpexercices.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+		System.out.println("Spring boot prject started");
+		System.out.println("welcome");
+	}
+
+}
