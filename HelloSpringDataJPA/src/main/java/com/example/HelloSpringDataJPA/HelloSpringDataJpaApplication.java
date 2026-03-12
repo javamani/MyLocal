9512fafee441c07fc8be6de4ba8158f713450373
javamani <pioneermani@yahoo.com>
2026-03-11 20:56:18 -0400

    Checkin Mar 11 using intellij

--- HelloSpringDataJPA/src/main/java/com/example/HelloSpringDataJPA/HelloSpringDataJpaApplication.java
+++ HelloSpringDataJPA/src/main/java/com/example/HelloSpringDataJPA/HelloSpringDataJpaApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HelloSpringDataJpaApplication {
 
 	public static void main(String[] args) {
+		System.out.println(" Spring boot data JPA");
 		SpringApplication.run(HelloSpringDataJpaApplication.class, args);
 	}
 
